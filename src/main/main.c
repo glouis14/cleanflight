01a0076469dced3e018745bd31799346b37e1866
Sukandar Kartadinata <sk@glui.de>
2016-10-21 22:15:12 +0200

    Added small comment

--- src/main/main.c
+++ src/main/main.c
@@ -13,6 +13,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Forked and renamed gluiflight
  */
 
 #include <stdbool.h>
